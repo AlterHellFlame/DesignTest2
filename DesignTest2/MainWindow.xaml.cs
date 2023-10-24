093a2298bf16ecbecabf0fbb0829b0127ce8fe7f
user <user@DESKTOP-LOAQH83>
2023-10-24 19:15:12 +0300

    Добавьте файлы проекта.

---
+++ DesignTest2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesignTest2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DataClassesDataContext dC1DC = new DataClassesDataContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Menu_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Clients_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Table.ItemsSource = null;
+            Table.ItemsSource = from client in dC1DC.Clients select client;
+        }
+
+        private void Service_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Table.ItemsSource = null;
+            Table.ItemsSource = from client in dC1DC.Service select client;
+        }
+
+        private void Work_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Table.ItemsSource = null;
+            Table.ItemsSource = from client in dC1DC.Work select client;
+        }
+
+    }
+}
---
+++ DesignTest2/MyData/Menu.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesignTest2.MyData
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : UserControl
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        //Текст
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string), typeof(Menu));
+
+        //Активен ли пункт
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static DependencyProperty IsActiveProperty =
+            DependencyProperty.Register("IsActive", typeof(bool), typeof(Menu));
+
+        //Иконка
+        public MahApps.Metro.IconPacks.PackIconMaterialKind Icon
+        {
+            get { return (MahApps.Metro.IconPacks.PackIconMaterialKind)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static DependencyProperty IconProperty =
+            DependencyProperty.Register("Icon", typeof(MahApps.Metro.IconPacks.PackIconMaterialKind), typeof(Menu));
+
+    }
+}
